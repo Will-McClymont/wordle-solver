159ea2a900a3304d784d13253ec2790fcea156b6
Will <wjm50@cam.ac.uk>
2023-01-04 10:25:51 +0000

    Added the word list and a function to fetch it.

--- solver.py
+++ solver.py
@@ -1,1 +1,13 @@
+"""
+Main solver module.
+"""
+
 import numpy as np
+
+
+def fetchWordList(path=r'all_words.txt'):
+
+    with open(path, 'r') as file:
+        words = [line.rstrip() for line in file]
+
+    return words
