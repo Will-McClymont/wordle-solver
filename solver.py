555c468f9aa158908e272aacb89c3a4d8a5bb08e
Will <wjm50@cam.ac.uk>
2023-01-05 10:57:00 +0000

    Working basic ssolver in a class structure. Suggests guesses of random viable words. 2 minimal tests check high level performance.

--- solver.py
+++ solver.py
@@ -5,9 +5,147 @@ Main solver module.
 import numpy as np
 
 
-def fetchWordList(path=r'all_words.txt'):
+class WordleSolver():
 
-    with open(path, 'r') as file:
-        words = [line.rstrip() for line in file]
+    def __init__(self,
+                 wordlist_path=r'all_words.txt',
+                 true_word=None
+                 ):
+        self.wordlist_path = wordlist_path
+        self.fetchWordList()
+        self.viable_wordlist = self.master_wordlist[:]
 
-    return words
+        self.num_attempts = 0
+        self.known_letters = ['*', '*', '*', '*', '*']
+        self.known_falseletters = [[], [], [], [], []]
+        self.minnum_letters = np.zeros(26, dtype=int)
+        self.maxnum_letters = np.ones(26, dtype=int) * 5
+
+        self.setTrueWord(true_word)
+
+    def setTrueWord(self, true_word):
+        '''
+        Sets the true_word attribute to the true_word provided.
+        Sets to a random word on the master list if true_word is None or
+        invalid.
+        '''
+
+        if true_word is None:
+            number_of_words = len(self.master_wordlist)
+            random_index = np.random.randint(number_of_words)
+            self.true_word = self.master_wordlist[random_index]
+
+        elif true_word in self.master_wordlist:
+            self.true_word = true_word
+
+        else:
+            print('Input true word not valid. Setting random master word.')
+            number_of_words = len(self.master_wordlist)
+            random_index = np.random.randit(number_of_words)
+            self.true_word = self.master_wordlist[random_index]
+
+        self.true_num_letters = np.zeros(26, dtype=int)
+        for i, letter in enumerate(self.true_word):
+            self.true_num_letters[ord(letter) - ord('a')] += 1
+
+    def fetchWordList(self):
+        '''
+        Fetches the master word list from the specificed file and returns the
+        words as a list.
+        '''
+        with open(self.wordlist_path, 'r') as file:
+            self.master_wordlist = [line.rstrip() for line in file]
+
+        self.wordlist_num_letters = np.zeros((len(self.master_wordlist), 26),
+                                             dtype=int)
+        for i, word in enumerate(self.master_wordlist):
+            for letter in word:
+                self.wordlist_num_letters[i, ord(letter) - ord('a')] += 1
+
+    def processGuess(self, guess):
+
+        # Check that the guess is a valid word
+        if guess not in self.master_wordlist:
+            print('Guess was not a valid 5 letter word. Guess not processed.')
+            return False
+        else:
+            self.num_attempts += 1
+
+        # Check if the guess is correct
+        if guess is self.true_word:
+            print(
+                f'You have successfully guessed the word after {self.num_attempts} attempts!')
+            return True
+
+        guess_num_letters = np.zeros(26, dtype=int)
+        # Update the lists with new knowledge from guess
+        for i, letter in enumerate(guess):
+            guess_num_letters[ord(letter) - ord('a')] += 1
+            if letter is self.true_word[i]:
+                self.known_letters[i] = letter
+            else:
+                self.known_falseletters[i].append(letter)
+
+        too_many_mask = guess_num_letters > self.true_num_letters
+        exact_mask = guess_num_letters == self.true_num_letters
+        too_few_mask = guess_num_letters < self.true_num_letters
+
+        # When you guess too many of a letter, you know the exact number of
+        # that letter in the true word.
+        print(guess_num_letters)
+        print(self.minnum_letters)
+        self.minnum_letters = np.where(too_many_mask,
+                                       self.true_num_letters,
+                                       self.minnum_letters)
+
+        self.maxnum_letters = np.where(too_many_mask,
+                                       self.true_num_letters,
+                                       self.maxnum_letters)
+
+        # When you guess the correct number or too few of a letter,
+        # you have updated knowledge about the minimum number of that letter,
+        # but only if your new knowledge
+        # is more restrictive than the previous knowledge gained.
+
+        self.minnum_letters = np.where(too_few_mask | exact_mask,
+                                       np.maximum(guess_num_letters,
+                                                  self.minnum_letters),
+                                       self.minnum_letters)
+
+        print(self.minnum_letters)
+        print(self.maxnum_letters)
+        self.eliminateNonViableWords()
+
+        return False
+
+    def eliminateNonViableWords(self):
+
+        for i, word in enumerate(list(self.viable_wordlist)):
+            # Check that the word doesn't violate known letter numbers
+            # THIS NEEDS TO BE DFIXED TO USE VIABLEWORDLIST_NUM_LETTERS
+            if np.any(self.wordlist_num_letters[i, :] < self.minnum_letters) or np.any(self.wordlist_num_letters[i, :] > self.maxnum_letters):
+                self.viable_wordlist.remove(word)
+                continue
+
+            for j, letter in enumerate(word):
+
+                # Check that the word contains the true letter, if known
+                if self.known_letters[j] != '*' and self.known_letters[j] != letter:
+                    self.viable_wordlist.remove(word)
+                    break
+
+                # Otherwise, check that the letter isn't known to be false
+                elif any(letter in x for x in self.known_falseletters[j][:]):
+                    self.viable_wordlist.remove(word)
+                    break
+
+        self.wordlist_num_letters = np.zeros((len(self.viable_wordlist), 26),
+                                             dtype=int)
+        for i, word in enumerate(self.viable_wordlist):
+            for letter in word:
+                self.wordlist_num_letters[i, ord(letter) - ord('a')] += 1
+
+    def suggestGuess(self):
+        number_of_words = len(self.viable_wordlist)
+        random_index = np.random.randint(number_of_words)
+        return self.viable_wordlist[random_index]
