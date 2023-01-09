dc5c6326443f5519d596283997cd61dbf1831591
Will <wjm50@cam.ac.uk>
2023-01-09 14:13:00 +0000

    Improved guessing method, input handling, and compliance with PEP-8.
    suggest_eliminator_guess method added to WordleSolver class. Suggests the
    guess that can eliminate the most common words in the remaining viable
    words.
    
    suggest_default_first_guess method suggests the guess 'shoal'. This method
    may be useful in future for suggesting a known best first guess.
    
    sanitise_word will allow uppercase input and inputs containing spaces to
    be converted to all lowercase and removes spaces. This method also checks
    that the word is 5 letters and contained in the master wordlist.
    
    Class methods renamed as lowercase_underscore rather than mixedCase in
    order to comply with PEP8 style.
    
     # Please enter the commit message for your changes. Lines starting

--- solver.py
+++ solver.py
@@ -12,7 +12,7 @@ class WordleSolver():
                  true_word=None
                  ):
         self.wordlist_path = wordlist_path
-        self.fetchWordList()
+        self.fetch_word_list()
         self.viable_wordlist = self.master_wordlist[:]
 
         self.num_attempts = 0
@@ -21,9 +21,31 @@ class WordleSolver():
         self.minnum_letters = np.zeros(26, dtype=int)
         self.maxnum_letters = np.ones(26, dtype=int) * 5
 
-        self.setTrueWord(true_word)
+        self.set_true_word(true_word)
 
-    def setTrueWord(self, true_word):
+    def sanitise_word(self, word):
+        '''
+        Sanitises an input word by removing spaces and converting to lower
+        case.
+        '''
+        sanitised_word = word.replace(" ", "")
+        sanitised_word = sanitised_word.lower()
+
+        if len(sanitised_word) != 5:
+            raise ValueError(f'Unable to convert {word} to a five letter lowercase word.')
+
+        if sanitised_word not in self.master_wordlist:
+            raise ValueError(f'Input word \'{word}\' is not in the Wordle dictionary.')
+
+        return sanitised_word
+
+    def output_word(self, word):
+        '''
+        Converts an all lower case word to all caps and provides a list of
+        values corresponding to the letter colour.
+        '''
+
+    def set_true_word(self, true_word):
         '''
         Sets the true_word attribute to the true_word provided.
         Sets to a random word on the master list if true_word is None or
@@ -48,7 +70,7 @@ class WordleSolver():
         for i, letter in enumerate(self.true_word):
             self.true_num_letters[ord(letter) - ord('a')] += 1
 
-    def fetchWordList(self):
+    def fetch_word_list(self):
         '''
         Fetches the master word list from the specificed file and returns the
         words as a list.
@@ -56,13 +78,16 @@ class WordleSolver():
         with open(self.wordlist_path, 'r') as file:
             self.master_wordlist = [line.rstrip() for line in file]
 
-        self.wordlist_num_letters = np.zeros((len(self.master_wordlist), 26),
-                                             dtype=int)
+        self.master_wordlist_num_letters = np.zeros((len(self.master_wordlist), 26),
+                                                    dtype=int)
         for i, word in enumerate(self.master_wordlist):
             for letter in word:
-                self.wordlist_num_letters[i, ord(letter) - ord('a')] += 1
+                self.master_wordlist_num_letters[i, ord(
+                    letter) - ord('a')] += 1
 
-    def processGuess(self, guess):
+        self.wordlist_num_letters = np.copy(self.master_wordlist_num_letters)
+
+    def process_guess(self, guess):
 
         # Check that the guess is a valid word
         if guess not in self.master_wordlist:
@@ -92,8 +117,7 @@ class WordleSolver():
 
         # When you guess too many of a letter, you know the exact number of
         # that letter in the true word.
-        print(guess_num_letters)
-        print(self.minnum_letters)
+
         self.minnum_letters = np.where(too_many_mask,
                                        self.true_num_letters,
                                        self.minnum_letters)
@@ -114,11 +138,11 @@ class WordleSolver():
 
         print(self.minnum_letters)
         print(self.maxnum_letters)
-        self.eliminateNonViableWords()
+        self.eliminate_nonviable_words()
 
         return False
 
-    def eliminateNonViableWords(self):
+    def eliminate_nonviable_words(self):
 
         for i, word in enumerate(list(self.viable_wordlist)):
             # Check that the word doesn't violate known letter numbers
@@ -145,7 +169,54 @@ class WordleSolver():
             for letter in word:
                 self.wordlist_num_letters[i, ord(letter) - ord('a')] += 1
 
-    def suggestGuess(self):
+    def suggest_random_guess(self):
         number_of_words = len(self.viable_wordlist)
         random_index = np.random.randint(number_of_words)
         return self.viable_wordlist[random_index]
+
+    def suggest_eliminator_guess(self, force_viable=False):
+        '''
+        Suggest a guess to eliminate as many common letters as possible.
+
+        parameters:
+        force_viable - Set to true in order to only select words that may still
+                       be the correct word.
+        '''
+
+        # Score number of words in which each letter appears
+
+        wordlist_binary_letters = np.where(self.wordlist_num_letters > 0, 1, 0)
+        letter_freq_list = np.sum(wordlist_binary_letters, axis=0)
+        letter_freq_list = np.where(
+            self.minnum_letters > 0, 0, letter_freq_list)
+        letter_freq_list = np.where(
+            self.maxnum_letters == 0, 0, letter_freq_list)
+        letter_freq_list = np.where(
+            self.maxnum_letters == self.minnum_letters, 0, letter_freq_list)
+
+        # Score the wordlist by multiplying each of its letters by their
+        # frequency. Double letters are only counted once.
+        if not force_viable:
+            wordlist_binary_letters = np.where(
+                self.master_wordlist_num_letters > 0, 1, 0)
+
+        wordlist_scores = np.sum(
+            wordlist_binary_letters * letter_freq_list, axis=1)
+        highest_scoring_index = np.argmax(wordlist_scores)
+        print(np.shape(wordlist_scores))
+        print(highest_scoring_index)
+        print(len(self.viable_wordlist))
+
+        if force_viable:
+            return self.viable_wordlist[highest_scoring_index]
+        elif not force_viable:
+            return self.master_wordlist[highest_scoring_index]
+
+    def suggest_default_first_guess(self):
+        return 'shoal'
+
+    def suggest_smart_guess(self):
+        '''
+        Suggest a guess from the list of viable remaining words based on a
+        criteria for which word is most likley.
+        '''
