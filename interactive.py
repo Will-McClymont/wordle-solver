8e11aabcf253568811227d645e95449374dad794
Will <wjm50@cam.ac.uk>
2023-01-13 15:00:39 +0000

    Added licence.

--- interactive.py
+++ interactive.py
@@ -5,6 +5,15 @@ order to play Wordle interactively.
 
 import userfunctions
 
+print('wordle-solver  Copyright (C) 2023 William McClymont')
+print('This program comes with ABSOLUTELY NO WARRANTY.')
+print('This is free software, and you are welcome to redistribute it')
+print('under certain conditions. See licence.txt for details.')
+print(' ')
+print('--------------------------------------------------------------')
+print(' ')
+print('Welcome to the interactive wordle-solver.')
+print(' ')
 print('Would you like suggestions? (Y/N)')
 suggest = input()
 if suggest.lower() == 'y':
